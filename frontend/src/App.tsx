3472b9b60efb0e953d309331a6101d40608fd1e1
laksamba <sandeshlaksamba123@gmail.com>
2025-07-01 16:45:51 +0545

    frontend setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
 
-  return (<>
+  return (<>  
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/dashboard" element={<div>Dashboard</div>} />
+      </Routes>
+    </BrowserRouter>
+  
   </>
   )}
 
